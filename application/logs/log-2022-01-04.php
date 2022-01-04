ac5a7f1d23020fc71912a78c3f75ac9d7ef992a6
vineet-kushwaha <55151140+vineet-kushwaha@users.noreply.github.com>
2022-01-04 15:48:06 +0530

    edit otherPackage edit0

---
+++ application/logs/log-2022-01-04.php
@@ -0,0 +1,94 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2022-01-04 15:33:38 --> Config Class Initialized
+INFO - 2022-01-04 15:33:38 --> Hooks Class Initialized
+DEBUG - 2022-01-04 15:33:38 --> UTF-8 Support Enabled
+INFO - 2022-01-04 15:33:38 --> Utf8 Class Initialized
+INFO - 2022-01-04 15:33:38 --> URI Class Initialized
+DEBUG - 2022-01-04 15:33:38 --> No URI present. Default controller set.
+INFO - 2022-01-04 15:33:38 --> Router Class Initialized
+INFO - 2022-01-04 15:33:38 --> Output Class Initialized
+INFO - 2022-01-04 15:33:38 --> Security Class Initialized
+DEBUG - 2022-01-04 15:33:38 --> Global POST, GET and COOKIE data sanitized
+INFO - 2022-01-04 15:33:38 --> CSRF cookie sent
+INFO - 2022-01-04 15:33:38 --> Input Class Initialized
+INFO - 2022-01-04 15:33:38 --> Language Class Initialized
+INFO - 2022-01-04 15:33:38 --> Loader Class Initialized
+INFO - 2022-01-04 15:33:39 --> Helper loaded: files_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: url_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: file_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: form_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: action_hooks_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: user_meta_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: emails_tracking_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: general_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: misc_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: func_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: gdpr_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: datatables_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: custom_fields_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: defaults_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: merge_fields_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: app_html_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: email_templates_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: invoices_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: subscriptions_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: expenses_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: estimates_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: contracts_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: credit_notes_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: proposals_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: projects_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: tasks_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: fields_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: leads_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: tickets_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: relation_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: tags_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: pdf_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: clients_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: database_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: upload_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: sales_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: themes_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: theme_style_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: pre_query_data_formatters_helper
+INFO - 2022-01-04 15:33:39 --> Helper loaded: widgets_helper
+INFO - 2022-01-04 15:33:40 --> Helper loaded: sms_helper
+INFO - 2022-01-04 15:33:40 --> Helper loaded: deprecated_helper
+INFO - 2022-01-04 15:33:40 --> Helper loaded: system_messages_helper
+INFO - 2022-01-04 15:33:40 --> Language file loaded: language/english/english_lang.php
+INFO - 2022-01-04 15:33:40 --> Database Driver Class Initialized
+INFO - 2022-01-04 15:33:40 --> Session: Class initialized using 'database' driver.
+INFO - 2022-01-04 15:33:40 --> User Agent Class Initialized
+INFO - 2022-01-04 15:33:40 --> Image Lib Class Initialized
+DEBUG - 2022-01-04 15:33:40 --> Encryption: Auto-configured driver 'openssl'.
+INFO - 2022-01-04 15:33:40 --> Encryption: OpenSSL initialized with method AES-128-CBC.
+INFO - 2022-01-04 15:33:40 --> Encryption Class Initialized
+INFO - 2022-01-04 15:33:41 --> Helper loaded: email_helper
+INFO - 2022-01-04 15:33:41 --> Helper loaded: html_helper
+INFO - 2022-01-04 15:33:41 --> Email Class Initialized (Engine: phpmailer)
+DEBUG - 2022-01-04 15:33:41 --> App class already loaded. Second attempt ignored.
+INFO - 2022-01-04 15:33:41 --> CRM_Model class loaded
+INFO - 2022-01-04 15:33:41 --> Model "Misc_model" initialized
+INFO - 2022-01-04 15:33:41 --> Model "Roles_model" initialized
+INFO - 2022-01-04 15:33:41 --> Model "Client_vault_entries_model" initialized
+INFO - 2022-01-04 15:33:41 --> Model "Client_groups_model" initialized
+INFO - 2022-01-04 15:33:41 --> Model "Statement_model" initialized
+INFO - 2022-01-04 15:33:41 --> Model "Clients_model" initialized
+INFO - 2022-01-04 15:33:41 --> Model "Projects_model" initialized
+INFO - 2022-01-04 15:33:41 --> Model "Staff_model" initialized
+INFO - 2022-01-04 15:33:41 --> Model "Tasks_model" initialized
+INFO - 2022-01-04 15:33:41 --> Model "Dashboard_model" initialized
+INFO - 2022-01-04 15:33:41 --> Controller Class Initialized
+DEBUG - 2022-01-04 15:33:41 --> Config file loaded: C:\xampp-5.6\htdocs\bfcgroup_liveGit\application\config/migration.php
+INFO - 2022-01-04 15:33:41 --> Language file loaded: language/english/english_lang.php
+INFO - 2022-01-04 15:33:41 --> Model "User_Autologin" initialized
+INFO - 2022-01-04 15:33:41 --> Helper loaded: cookie_helper
+INFO - 2022-01-04 15:33:41 --> Model "Authentication_model" initialized
+INFO - 2022-01-04 15:33:41 --> Form Validation Class Initialized
+INFO - 2022-01-04 15:33:42 --> File loaded: C:\xampp-5.6\htdocs\bfcgroup_liveGit\application\views\authentication/includes/head.php
+INFO - 2022-01-04 15:33:42 --> File loaded: C:\xampp-5.6\htdocs\bfcgroup_liveGit\application\views\authentication/includes/alerts.php
+INFO - 2022-01-04 15:33:42 --> File loaded: C:\xampp-5.6\htdocs\bfcgroup_liveGit\application\views\authentication/login_admin.php
+INFO - 2022-01-04 15:33:42 --> Final output sent to browser
+DEBUG - 2022-01-04 15:33:42 --> Total execution time: 3.6381
